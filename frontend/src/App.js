d4ecc8473dfbb650f6e39bc94b9b87862e9de5d7
Cameron Rasmussen <cameron.p.rasmussen@gmail.com>
2020-03-12 16:08:40 -0600

    signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SignIn from './components/SignIn.js';
 
 class App extends React.Component{
     render(){
         return(
             <div>
-                
+                <SignIn/>
             </div>
         )
     }
---
+++ frontend/src/components/SignIn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SignIn extends React.Component{
+    render(){
+        return(
+            <div id="sign-in-container">
+                <div id="sign-in-header">
+                    <h4>Sign In</h4>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
